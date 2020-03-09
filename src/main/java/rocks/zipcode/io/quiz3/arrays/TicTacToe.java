c78b669379f1e69ae2208fc0012f3c8c3b9918b5
asconem <asconem@gmail.com>
2020-03-09 11:24:43 -0400

    Working on TicTacToe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -4,23 +4,28 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    private String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        String[] row = {board[value][0], board[value][1], board[value][2]};
+        return row;
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] column = {board[0][value], board[1][value], board[2][value]};
+        return column;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        return (getRow(rowIndex)[0].equals("X") && getRow(rowIndex)[1].equals("X") && getRow(rowIndex)[2].equals("X")) ||
+                (getRow(rowIndex)[0].equals("O") && getRow(rowIndex)[1].equals("O") && getRow(rowIndex)[2].equals("O"));
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
