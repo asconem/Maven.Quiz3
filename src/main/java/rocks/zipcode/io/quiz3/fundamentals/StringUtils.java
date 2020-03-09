07c7af720c4628060f9665eb5efa3c81a28e1bf5
asconem <asconem@gmail.com>
2020-03-09 10:36:53 -0400

    Updating progress

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -1,25 +1,51 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+        char[] newArr = str.toCharArray();
+        newArr[indexToCapitalize] = Character.toUpperCase((newArr[indexToCapitalize]));
+        return new String(newArr);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+        for (int i = 0; i < baseString.length(); i++) {
+            if(baseString.toCharArray()[indexOfString] == characterToCheckFor) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        int count = string.length() * (string.length() + 1) / 2;
+        Set<String> sub = new HashSet<>(count);
+        for(int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j<= string.length(); j++) {
+                sub.add(string.substring(i, j));
+            }
+        }
+        return sub.toArray(new String[sub.size()]);
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+        int count = input.length() * (input.length() + 1) / 2;
+        int sub = -1;
+        ArrayList<String> substrings = new ArrayList<>(count);
+
+        for (int i = 0; i< input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                substrings.add(input.substring(i, j));
+                sub++;
+            }
+        }
+        return sub;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -4,20 +4,58 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
+    private static String vowels = "aeiou";
+
     public static Boolean hasVowels(String word) {
-        return null;
+        char[] lower = word.toLowerCase().toCharArray();
+        char[] newArr = vowels.toCharArray();
+        Boolean check = false;
+
+        for (int i = 0; i < newArr.length; i++) {
+            for (int j = 0; j < lower.length; j++) {
+                if (lower[j] == newArr[i]) {
+                    check = true;
+                    break;
+                }
+            }
+            if(check){
+                break;
+            }
+        }
+        return check;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        char[] lower = word.toLowerCase().toCharArray();
+        for (int i = 0; i < lower.length; i++) {
+            for (int j = 0; j < vowels.length(); j++) {
+                if (lower[i] == vowels.charAt(j)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        String lower = word.toLowerCase();
+        for (int i = 0; i < vowels.length(); i++) {
+            if (vowels.charAt(i) == lower.charAt(0)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        Character lower = Character.toLowerCase(character);
+        Boolean check = false;
+        for (int i = 0; i < vowels.length(); i++) {
+            if(lower == vowels.charAt(i)) {
+                check = true;
+            }
+        }
+        return check;
     }
 }
