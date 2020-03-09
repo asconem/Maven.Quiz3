71914bd01ad24878c557940027b663b3e4fdd31f
asconem <asconem@gmail.com>
2020-03-09 09:41:40 -0400

    Initial commit

--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,30 +1,47 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+    private List<Lab> labs;
 
     public Student() {
         this(null);
     }
 
     public Student(List<Lab> labs) {
+        this.labs = new ArrayList<>();
     }
 
     public Lab getLab(String labName) {
-        return null;
+        Lab lab = null;
+        for(int i = 0; i < labs.size(); i++) {
+            if(labs.get(i).getName().equals(labName)) {
+                lab = labs.get(i);
+                break;
+            }
+        }
+        return lab;
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        try {
+            getLab(labName).setStatus(labStatus);
+        } catch (Exception e) {
+            throw new UnsupportedOperationException(e);
+        }
     }
 
     public void forkLab(Lab lab) {
+        lab.setStatus(LabStatus.PENDING);
+        labs.add(lab);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+        return getLab(labName).getStatus();
     }
 }
